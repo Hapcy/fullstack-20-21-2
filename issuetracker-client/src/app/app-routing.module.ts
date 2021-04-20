fbca374cffeb659a8b53b746896a4103311bb1d6
tibor.moger <tibor.moger@smartx.eu>
2021-04-20 17:31:10 +0200

    angular basics, angular material

--- issuetracker-client/src/app/app-routing.module.ts
+++ issuetracker-client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
+import { IssuesComponent } from './issues/issues.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'issues',
+  component: IssuesComponent,
+}, {
+  path: 'dummy',
+  component: DummyComponent,
+}, {
+  path: '**',
+  redirectTo: '/issues',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- issuetracker-client/src/app/app.module.ts
+++ issuetracker-client/src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DummyComponent } from './dummy/dummy.component';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IssuesComponent } from './issues/issues.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DummyComponent,
+    MenuComponent,
+    IssuesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
